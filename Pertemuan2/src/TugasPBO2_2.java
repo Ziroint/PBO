ee45a1d2a9d8f62971bffd2d4b591c9efcf046d5
Ziroint <112533265+Ziroint@users.noreply.github.com>
2022-10-11 11:40:35 +0700

    Update TugasPBO2_2.java

--- Pertemuan2/src/TugasPBO2_2.java
+++ Pertemuan2/src/TugasPBO2_2.java
@@ -1,14 +1,18 @@
 import java.util.Scanner;
 public class TugasPBO2_2 {
     public static void main(String args[]) {
+        // menggunakan while untuk perulangan benar
         while(true){
+            // variable kata untuk tipe data string
             String kata;
             kata = "Kalau dilakukan dengan sepenuh hati maka besi batangan pun kalau digosok terus menerus akan menjadi jarum";
             System.out.println("");
             String huruf;
+            // scanner input, untuk menginput kata yang ada
             Scanner input = new Scanner(System.in);
             System.out.print("Masukkan kata yang sesuai:");
             huruf = input.nextLine();
+            // saya menggunakan contains agar bisa mengambil per kata dalam variabel kata
             if (kata.contains(huruf)) {
                 System.out.println("Inputan benar");
                 break;
