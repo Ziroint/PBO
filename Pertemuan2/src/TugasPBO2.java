3cac675e0f31a4c1a4dc70e5afc030408fd0d2b8
Ziroint <112533265+Ziroint@users.noreply.github.com>
2022-10-11 11:38:47 +0700

    Update TugasPBO2.java

--- Pertemuan2/src/TugasPBO2.java
+++ Pertemuan2/src/TugasPBO2.java
@@ -1,15 +1,20 @@
 import java.util.Scanner;
 public class TugasPBO2 {
     public static void main(String args[]){
+        // membuat variabel angka dan jwb dengan tipe data integer dan string.
         int angka;
         String jwb;
+        // scanner input digunakan untuk menginput nilai
         Scanner input = new Scanner(System.in);
+        // menggunakan boolean lanjut untuk mengetahui mana true dan false
         boolean lanjut;
+        // menggunakan do while untuk perulangan
         do {
             System.out.print("Masukkan nilai dari 0 - 10 :");
             angka = input.nextInt();
 
 
+            // if else untuk memasukkan data, jika salah maka input ulang, jika benar maka akan ditanya kembali
             if (angka <= 10 && angka >= 0) {
                 System.out.println("Angka yang anda masukkan adalah: " + angka);
 
@@ -18,7 +23,8 @@ public class TugasPBO2 {
             }
 
             input.nextLine();
-
+            
+            // jika nilai true
             while (true) {
                 System.out.print("Apakah mau lanjut[Y/T]?");
                 jwb = input.nextLine().toUpperCase();
