53bf01778b488bd7e3b5ceac79585198a4b3e445
Ziroint <112533265+Ziroint@users.noreply.github.com>
2022-12-23 21:16:34 +0700

    Add files via upload

---
+++ main/java/com/example/progressuas/login_akun.sql
@@ -0,0 +1,56 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Dec 23, 2022 at 02:16 PM
+-- Server version: 8.0.30
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `stok_barang`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login_akun`
+--
+
+CREATE TABLE `login_akun` (
+  `idUsername` int NOT NULL,
+  `Username` varchar(30) NOT NULL,
+  `password` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `login_akun`
+--
+
+INSERT INTO `login_akun` (`idUsername`, `Username`, `password`) VALUES
+(1, 'adminycs', 'admin123');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `login_akun`
+--
+ALTER TABLE `login_akun`
+  ADD PRIMARY KEY (`idUsername`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
